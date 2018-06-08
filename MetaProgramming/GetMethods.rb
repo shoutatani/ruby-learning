19853919927b2e99fcde2c7c81cb1d25b2f6afd2
S.Taniguchi <shoutatani@gmail.com>
2018-06-08 14:26:42 +0900

    Modified abou GetMethods.

--- MetaProgramming/GetMethods.rb
+++ MetaProgramming/GetMethods.rb
@@ -6,13 +6,19 @@ class Greeting
   def welcome
     @text
   end
+
+  def Greeting.pow
+    "x"
+  end
+
 end
 
-
-o = Greeting.new("Hello")
+o = Greeting.new('Hello')
 p o.class
-p o.class.instance_methods(false) # 自分に定義されたインスタンスメソッドの一覧だけ。継承したものは不要
-p o.class.instance_methods()
-p o.class.instance_methods(true)
+p o.class.instance_methods(false) # 自分に定義されたインスタンスメソッドの一覧だけ。継承したものは表示しない。
+p o.class.instance_methods  # 継承したインスタンスメソッドも表示
+p o.class.instance_methods(true) # 継承したインスタンスメソッドも表示
 
-p o.instance_variables
+p o.instance_variables # この時点までにセットされたインスタンス変数を表示
+p o.methods # 継承しているものも含めたメソッドの一覧を取得できる。publicかprotectedなものだけ。親クラス・インクルードしているモジュールのメソッドも含まれる。。
+# p o.class.methods(false) # 特異メソッドだけ。
